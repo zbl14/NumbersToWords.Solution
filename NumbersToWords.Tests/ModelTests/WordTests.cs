e1007cb04bb38cfb24e2c8f61f8e26bf7d47f059
zbl14 <ifthereisoneday@gmail.com>
2022-07-14 14:55:50 -0700

    return Eleven for 11, test passed

--- NumbersToWords.Tests/ModelTests/WordTests.cs
+++ NumbersToWords.Tests/ModelTests/WordTests.cs
@@ -15,10 +15,25 @@ namespace NumbersToWords.Tests
     [TestMethod]
     public void Ones_ReturnOnes_One()
     {
-      // Word testWord = new Word();
       string numStr = "1";
       string word = Word.ones(numStr);
       Assert.AreEqual("One", word);
+    }
+
+    [TestMethod]
+    public void Ones_ReturnOnes_Nine()
+    {
+      string numStr = "9";
+      string word = Word.ones(numStr);
+      Assert.AreEqual("Nine", word);
+    }
+
+    [TestMethod]
+    public void Tens_ReturnTens_Eleven()
+    {
+      string numStr = "11";
+      string word = Word.tens(numStr);
+      Assert.AreEqual("Eleven", word);
     }
   }
 }
--- NumbersToWords/Models/Word.cs
+++ NumbersToWords/Models/Word.cs
@@ -13,6 +13,100 @@ namespace NumbersToWords.Models
         case 1:
           name = "One";
           break;
+        case 2:
+          name = "Two";
+          break;
+        case 3:
+          name = "Three";
+          break;
+        case 4:
+          name = "Four";
+          break;
+        case 5:
+          name = "Five";
+          break;
+        case 6:
+          name = "Six";
+          break;
+        case 7:
+          name = "Seven";
+          break;
+        case 8:
+          name = "Eight";
+          break;
+        case 9:
+          name = "Nine";
+          break;
+        case 0:
+          name = "Zero";
+          break;
+      } 
+      return name;
+    }
+
+    public static string tens(string numStr)
+    {
+      Num = int.Parse(numStr);
+      string name = null;
+      switch (Num)
+      {
+        case 10:
+          name = "Ten";
+          break;
+        case 11:
+          name = "Eleven";
+          break;
+        case 12:
+          name = "Tweleve";
+          break;
+        case 13:
+          name = "Thirteen";
+          break;
+        case 14:
+          name = "Forteen";
+          break;
+        case 15:
+          name = "Fifthteen";
+          break;
+        case 16:
+          name = "Sixteen";
+          break;
+        case 17:
+          name = "Seventeen";
+          break;
+        case 18:
+          name = "Eighteen";
+          break;
+        case 19:
+          name = "Nineteen";
+          break;
+        case 20:
+          name = "Twenty";
+          break;
+        case 30:
+          name = "Thirty";
+          break;
+        case 40:
+          name = "Forty";
+          break;
+        case 50:
+          name = "Fifty";
+          break;
+        case 60:
+          name = "Sixty";
+          break;
+        case 70:
+          name = "Seventy";
+          break;
+        case 80:
+          name = "Eighty";
+          break;
+        case 90:
+          name = "Ninety";
+          break;
+        default:
+          name = tens(numStr.Substring(0, 1) + "0") + " " + ones(numStr.Substring(1));
+          break;          
       } 
       return name;
     }
